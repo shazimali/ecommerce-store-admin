3c3f6555becee5d33724fad36276c875fce8d8e4
shazim ali <shazimali03@gmail.com>
2025-02-20 11:55:07 +0500

    add quill rich text editor library

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,12 @@ import '@core/scss/template/index.scss'
 import { registerPlugins } from '@core/utils/plugins'
 import '@layouts/styles/index.scss'
 import '@styles/styles.scss'
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
 import 'vue3-toastify/dist/index.css'
+
 
 const app = createApp(App)
 registerPlugins(app)
+app.component('QuillEditor', QuillEditor)
 app.mount('#app')
