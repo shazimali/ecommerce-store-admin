df453ffda59b0e0660ca2fa49fc5be1c3140998a
Abid <abidranaabid90@gmail.com>
2025-04-23 13:39:36 +0500

    set price percentage from 1 to 100

--- src/components/ProductPrice.vue
+++ src/components/ProductPrice.vue
@@ -47,7 +47,7 @@
                 <v-col cols="4">
                     <v-select
                     v-model="form.discount"
-                    :items="[10.20,30,40.50,60,70,80,90,100]"
+                    :items="Array.from({ length: 100 }, (_, i) => i + 1)"
                     label="Discount"
                     />
                 </v-col>
